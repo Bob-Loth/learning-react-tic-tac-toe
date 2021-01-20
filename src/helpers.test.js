06c6ad72dece97941f0e1d0c4669758ff4fdb3f4
Bob-Loth <bob94loth@gmail.com>
2021-01-20 10:52:56 -0800

    added testing suite of calculateWinner

---
+++ src/helpers.test.js
@@ -0,0 +1,34 @@
+import {calculateWinner} from './helpers';
+
+test('should return X as the winner if it has the top row', () => {
+   // Arrange the test
+   const squares = ['X', 'X', 'X', 'O', null, 'O', null, null, null];
+   
+   // Act
+   const winner = calculateWinner(squares);
+ 
+   // Assert
+   expect(winner).toBe('X');
+ });
+
+ test('should return O as the winner if it has the ascending diagonal', () => {
+   // Arrange the test
+   const squares = ['X', 'X', 'O', null, 'O', null, 'O', null, null];
+   
+   // Act
+   const winner = calculateWinner(squares);
+ 
+   // Assert
+   expect(winner).toBe('O');
+ });
+
+ test('should return no winner if no lines have been made', () => {
+   // Arrange the test
+   const squares = ['X', null, 'X', 'O', null, 'O', null, null, null];
+   
+   // Act
+   const winner = calculateWinner(squares);
+ 
+   // Assert
+   expect(winner).toBe(null);
+ });
